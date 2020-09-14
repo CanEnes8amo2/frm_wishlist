47d62e24c16c283b49634a90c457cdd26558f1b3
EnesCan1039335 <1039335@mborijnland.nl>
2020-09-14 09:28:14 +0200

    initial commit

---
+++ versie2/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Wishlist</h1>
+            <p class="lead">Welkom op de webpagina! Hier kun je je eigen wishlist maken.</p>
+        </div>
+    </div>
+
+@endsection
