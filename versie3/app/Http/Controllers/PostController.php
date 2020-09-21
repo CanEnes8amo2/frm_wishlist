666b53d15973a13fc4c3fc47c7a00579b2547fe8
EnesCan1039335 <1039335@mborijnland.nl>
2020-09-21 09:12:51 +0200

    cloned versie2 into versie3

---
+++ versie3/app/Http/Controllers/PostController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index',compact('posts'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'picture' => 'required|image|max:2048',
+            'link' => 'required'
+        ]);
+
+        $picture = $request->file('picture');
+
+        $new_name = rand() . '.' . $picture->getClientOriginalExtension();
+        $picture->move(public_path('images'), $new_name);
+        $form_data = array(
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'picture' => $new_name,
+            'link' => $request->link
+        );
+
+        Post::create($form_data);
+
+        return redirect()->route('posts.index')->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show',compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+
+        return view('posts.edit',compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'picture' => 'required|image|max:2048',
+            'link' => 'required'
+        ]);
+
+        $picture = $request->file('picture');
+
+        $new_name = rand() . '.' . $picture->getClientOriginalExtension();
+        $picture->move(public_path('images'), $new_name);
+        $form_data = array(
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'picture' => $new_name,
+            'link' => $request->link
+        );
+
+        Post::create($form_data);
+
+        return redirect()->route('posts.index')->with('success','Post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success','post deleted successfully');
+    }
+
+    public function admin() {
+        $posts = Post::all();
+
+        return view('posts.admin',compact('posts'));
+    }
+}
